d5b243449d7b25be9fd3a9ad72e8bd1241173ae9
smoham2 <97156600+smoham2@users.noreply.github.com>
2025-10-31 00:32:09 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoic21vaG5qMjIiLCJhIjoiY21oZWZoaXJzMGRldDJxb3J0aHAxNnR4ciJ9.IA4TUQ5IFDJgFkifqfpdcw';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/light-v11', 
+  center: [138, 38],
+  zoom: 4.5
+});
+
+const japan = {
+  "type": "FeatureCollection",
+  "features": [{
+    "type": "Feature",
+    "geometry": { "type": "Polygon", "coordinates": [[[129,31],[146,31],[146,46],[129,46],[129,31]]] },
+    "properties": { "name": "Japan" }
+  }]
+};
+
+const earthquakes = {
+  "type": "FeatureCollection",
+  "features": [
+    { "type":"Feature","geometry":{"type":"Point","coordinates":[140,35]},"properties":{"id":1,"mag":5.2} },
+    { "type":"Feature","geometry":{"type":"Point","coordinates":[137,36]},"properties":{"id":2,"mag":6.1} }
+  ]
+};
+
+map.on('load', () => {
+  map.addSource('japan', { type: 'geojson', data: japan });
+  map.addLayer({
+    id: 'japan-fill', type: 'fill', source: 'japan',
+    paint: { 'fill-color': '#9ad0ff', 'fill-opacity': 0.35 }
+  });
+
+  map.addSource('eq', { type: 'geojson', data: earthquakes });
+  map.addLayer({
+    id: 'eq-points', type: 'circle', source: 'eq',
+    paint: { 'circle-radius': 7, 'circle-color': '#e84d4d', 'circle-stroke-width': 1, 'circle-stroke-color': '#fff' }
+  });
+});
